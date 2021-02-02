d912a5f49f39d816e5618975e072a0c616a5a9d2
LuvPatel <info.luvpatel@gmail.com>
2021-02-02 23:10:43 +0530

    database

--- dbtestapp/models.py
+++ dbtestapp/models.py
@@ -2,6 +2,24 @@ from django.db import models
 
 # Create your models here.
 
-class Student(models.Model):
-	student_name = models.CharField(max_length=100)
-	student_dob = models.DateTimeField('date published')
+class Customer(models.Model):
+	name=models.CharField(max_length=100)
+	address=models.TextField()
+	cust_id=models.AutoField(primary_key=True)
+	email=models.CharField(max_length=50)
+	mobile=models.IntegerField()
+
+class Seller(models.Model):
+	seller_name=models.CharField(max_length=100)
+	seller_address=models.TextField()
+	seller_id=models.AutoField(primary_key=True)
+	seller_email=models.CharField(max_length=50)
+	seller_mobile=models.IntegerField()
+
+class Product(models.Model):
+	prod_name=models.CharField(max_length=100)
+	prod_brand=models.CharField(max_length=30,primary_key=True)
+	prod_availability=models.BooleanField()
+	prod_category=models.CharField(max_length=50)
+	prod_price=models.IntegerField()
+
