86e458e5b217d7f4598214b512afd32320ae8625
LuvPatel <info.luvpatel@gmail.com>
2021-02-23 23:03:39 +0530

    Commit Message

---
+++ firstdbtest/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path,include
+from django.conf.urls import url
+from firstdb import views
+urlpatterns = [
+# path('', views.index, name='index'),
+    url(r'^addstudentinfo/$', views.addstudentinfo),
+    url(r'^delstudentinfo/$', views.delstudentinfo),
+    url(r'^getstudentinfo/$', views.getstudentinfo),
+    url(r'^addsuccess/$', views.addsuccess),
+    path('students/', views.StudentListView.as_view(), name = 'students'),
+]
